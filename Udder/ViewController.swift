69b0a01ca8ec90fcb03ce49ea6d7e00512380abd
Tim Goens <timcgoens@gmail.com>
2019-08-22 08:45:21 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tim Goens on 8/22/19.
+//  Copyright © 2019 Tim Goens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
